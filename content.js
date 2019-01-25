4397f343b782ca64b86c8d48da1ce8d1733d83df
kumaraditya1999 <kumaraditya1999@gmail.com>
2019-01-25 22:30:44 +0530

    almost complete only audio remaining

--- content.js
+++ content.js
@@ -1,140 +1,108 @@
-$('body').append("<div id=\"tooltip\"></div>");
+$('body').append("<div id=\"tooltip\" style=\"display :none;\">\
+	<div><span><i class=\"fa fa-close\" id=\"tooltip-icon\"></i></span></div>\
+	<div id=\"tooltip-body\"></div><hr>\
+	<div id=\"tooltip-button\"class=\"tooltip-group\"><i class=\"fa fa-angle-left\"></i><i class=\"fa fa-angle-right\"></i></div>\
+	</div>");
 
 
-// var object = {
-// 	"word" : word_analysis[i].word,
-// 	"type" : type,
-// 	"defination": word_analysis[i][type].defination,
-// 	"example": word_analysis[i][type].example,
-// 	"synonyms": word_analysis[i][type].synonyms
-// }
-// list.append(object);
-// console.log(object);
 
-$('body').append("<div id=\"tooltip\"></div>")
+//add the faslink
 
+var link = document.createElement( "link" );
+link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+document.getElementsByTagName( "head" )[0].appendChild( link );
 
 
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
-	console.log("here");
+
 	if(request.todo == "display meaning"){
-		// var color = '#'+request.clickedColor;
-		// var word_analysis = request.wordData;
-		// var list = []
-
-		// for(var i=0;i<word_analysis.lenght;i++){
-		// 	if(word_analysis[i].meaning.noun){
-		// 		for(var j=0;j<word_analysis[i].meaning.noun.lenght;j++){
-		// 			var object = {
-		// 				"word" : word_analysis[i].word,
-		// 				"type" : "noun",
-		// 				"defination": word_analysis[i].meaning.noun[j].defination,
-		// 			}
-		// 			list.append(object);
-		// 			console.log(object);
-		// 		}
-				
-		// 	}
-
-		// 	if(word_analysis[i].meaning.abbreviation){
-		// 		for(var j=0;j<word_analysis[i].meaning.abbreviation.lenght;j++){
-		// 			var object = {
-		// 				"word" : word_analysis[i].word,
-		// 				"type" : "abbreviation",
-		// 				"defination": word_analysis[i].meaning.abbreviation[j].defination,
-		// 			}
-		// 			list.append(object);
-		// 			console.log(object);
-		// 		}
-				
-		// 	}
-
-		// 	if(word_analysis[i].meaning.adjective){
-		// 		for(var j=0;j<word_analysis[i].meaning.adjective.lenght;j++){
-		// 			var object = {
-		// 				"word" : word_analysis[i].word,
-		// 				"type" : "adjective",
-		// 				"defination": word_analysis[i].meaning.adjective[j].defination,
-		// 			}
-		// 			list.append(object);
-		// 			console.log(object);
-		// 		}
-				
-		// 	}
-
-		// 	if(word_analysis[i].meaning.adjective){
-		// 		for(var j=0;j<word_analysis[i].meaning.adjective.lenght;j++){
-		// 			var object = {
-		// 				"word" : word_analysis[i].word,
-		// 				"type" : "adjective",
-		// 				"defination": word_analysis[i].meaning.adjective[j].defination,
-		// 			}
-		// 			list.append(object);
-		// 			console.log(object);
-		// 		}
-				
-		// 	}
-
-		// 	if(word_analysis[i].meaning.pronoun){
-		// 		for(var j=0;j<word_analysis[i].meaning.pronoun.lenght;j++){
-		// 			var object = {
-		// 				"word" : word_analysis[i].word,
-		// 				"type" : "pronoun",
-		// 				"defination": word_analysis[i].pronoun.adjective[j].defination,
-		// 			}
-		// 			list.append(object);
-		// 			console.log(object);
-		// 		}
-				
-		// 	}
-
-		// 	if(word_analysis[i].meaning.verb){
-		// 		for(var j=0;j<word_analysis[i].meaning.verb.lenght;j++){
-		// 			var object = {
-		// 				"word" : word_analysis[i].word,
-		// 				"type" : "verb",
-		// 				"defination": word_analysis[i].verb.adjective[j].defination,
-		// 			}
-		// 			list.append(object);
-		// 			console.log(object);
-		// 		}
-				
-		// 	}
-
-
-
-
-		// }
 		var list = []
 		for(var i =0;i<request.wordData.length;i++){
 			//console.log(request.wordData[i]);
 			for(var type in request.wordData[i].meaning){
 				//console.log(request.wordData[i].meaning[type]);
-				var object = {
-					"word" : request.wordData[i].word,
-					"example":request.wordData[i].meaning[type][0].example,
-					"definition":request.wordData[i].meaning[type][0].definition,
-					"type": type,
-					"synonyms":request.wordData[i].meaning[type][0].synonyms,
-				}
+				for(var j=0;j<request.wordData[i].meaning[type].length;j++){
+					var object = {
+						"word" : request.wordData[i].word,
+						"example":request.wordData[i].meaning[type][j].example,
+						"definition":request.wordData[i].meaning[type][j].definition,
+						"type": type,
+						"synonyms":request.wordData[i].meaning[type][j].synonyms,
+					}
 
-				//console.log(object);
-				list.push(object);
+					//console.log(object);
+					list.push(object);
+				}
+				
 			}
 		}
-		
+		//console.log(list)
 		var stringList = [];
 		
 		for(var i=0;i<list.length;i++){
 			var string = "";
-			
-		}
+			string  += "<div id=\"tooltip-title\" class=\"tooltip-group\"><span>"+ list[i].word +"</span><span><i class=\"fa fa-microphone\"></i></span></div><hr>\
+			<div id=\"tooltip-type\" class=\"tooltip-group\"><span>"+list[i].type + "</span></div>";
+			if(list[i].definition != undefined)
+			string += "<div id=\"tooltip-defination\" class=\"tooltip-group\"><span>definition: </span>"+list[i].definition+"</div>";
+			if(list[i].example != undefined)
+			string += "<div id=\"tooltip-example\" class=\"tooltip-group\"><span>example: </span>"+list[i].example+"</div>";
 
-		console.log(list);
-		console.log("changing");
-		$('p').css('color', 'blue' );
-		console.log($('body'));
-		$('body').append("<p>yo bitch</p>")
-		console.log("<p>yo bitch</p>");
+			if(list[i].synonyms !=  undefined ){
+				string+="<div id=\"tooltip-synonyms\" class=\"tooltip-group\"><span>synonyms:</span>";
+				//console.log(list[i])
+				
+				for(var j=0;j<list[i].synonyms.length;j++){
+					string+=list[i].synonyms[j]+",";
+					if(j%2){
+						string+="\n";
+					}
+				}
+				string+="</div>";
+			}
+		
+		
+		
+		stringList.push(string);
+		}
+		var i =0;
+		$('#tooltip').css({"display":"block"});
+		$("#tooltip-body").html(stringList[i]);
+		//add eventListeners 
+		$("#tooltip-icon").click(function(){
+			$('#tooltip').css({"display":"none"});
+		});
+
+		$(".fa-microphone").click(function(){
+			chrome.tts.speak(list[i].word,{'rate':0.8});
+		});
+
+		$(".fa-angle-right").click(function(){
+			i++;
+			if(i>=stringList.length){
+				i=0;
+			}
+			$("#tooltip-body").html(stringList[i]);
+			$(".fa-microphone").click(function(){
+				chrome.tts.speak(list[i].word,{'rate':0.8});
+			});
+
+		});
+
+		$(".fa-angle-left").click(function(){
+			i--;
+			if(i<0){
+				i=stringList.length-1;
+			}
+			$("#tooltip-body").html(stringList[i]);
+			$(".fa-microphone").click(function(){
+				chrome.tts.speak(list[i].word,{'rate':0.8});
+			});
+
+		});
+
 	}
 });
